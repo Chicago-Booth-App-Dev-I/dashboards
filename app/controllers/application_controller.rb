cd9fa8eed4de97e5a5b7457944b8641bdb8b0b6a
dhruv-parikh-booth <dhruv.parikh@chicagobooth.edu>
2021-05-04 01:32:18 +0000

    First currency WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+def forex_home
+  @raw_data = open("https://api.exchangerate.host/symbols").read
+  @parsed_data = JSON.parse(@raw_data)
+
+  @symbols_hash = @parsed_data.fetch("symbols")
+  @list_of_symbols = @symbols_hash.keys
+
+  render({:template => "forex/home.html.erb"})
+end
+
+
+
 end
