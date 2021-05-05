4df364507510bd34f47b14be10f1279f6e4f5a11
dhruv-parikh-booth <dhruv.parikh@chicagobooth.edu>
2021-05-05 14:54:18 +0000

    almost there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,10 @@ def to_convert
 
   @currency_choice = params.fetch("first_currency")
   @currency_convert = params.fetch("second_currency")
+  
+  @currency_conversion_raw_data = open("https://api.exchangerate.host/convert?from=USD&to=EUR").read
+  @currency_conversion_parsed_data = JSON.parse(@currency_conversion_raw_data)
+  @conversion_rate = @currency_conversion_parsed_data.fetch("info").fetch("rate")
 
   render({:template => "forex/second_currency.html.erb"})
 
